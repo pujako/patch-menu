6befd411396afe0c096c8894451bacdaa60b2a35
pujako <arrazzaq.zahari@gmail.com>
2024-07-26 23:36:44 +0800

    .

--- patch-menu_v1/functions/patch_server.py
+++ patch-menu_v1/functions/patch_server.py
@@ -81,5 +81,4 @@ def patch_server(stdscr, server_list):
         stdscr.addstr(len(server_list) + 1, 0, f"Patching complete. Results saved to {filepath}. Press any key to return to the menu.")
         stdscr.refresh()
         stdscr.getch()
-        stdscr.clear()
 
