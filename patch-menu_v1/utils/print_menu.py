e9d01d5de207ac95be227961970048e1e9531dd8
pujako <arrazzaq.zahari@gmail.com>
2024-07-27 02:39:45 +0800

    .

--- patch-menu_v1/utils/print_menu.py
+++ patch-menu_v1/utils/print_menu.py
@@ -2,7 +2,8 @@ import curses
 import time
 from datetime import datetime
 
-def print_menu(stdscr, selected_row_idx):
+def print_menu(stdscr, selected_row_idx, menu):
+    stdscr.clear()
     h, w = stdscr.getmaxyx()
 
     # Adding the title
@@ -26,11 +27,9 @@ def print_menu(stdscr, selected_row_idx):
     stdscr.attroff(curses.color_pair(2))
 
     # Defining the menu items
-    menu = ['Enter server list', 'List servers', 'Check server uptime', 'Gather server info', 'List repo files', 'Disable external repos', 'Enable external repos', 'Patch servers', 'Reboot servers', 'Exit']
-
     for idx, row in enumerate(menu):
         x = w // 2 - len(row) // 2
-        y = h // 2 - len(menu) // 2 + idx + 2 + 2  # +2 to account for the title, +2 for the date and time
+        y = h // 2 - len(menu) // 2 + idx + 4  # +4 to account for the title and date/time
         if idx == selected_row_idx:
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y, x, row)
@@ -51,9 +50,11 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     selected_row_idx = 0
 
+    menu = ['Enter server list', 'List servers', 'Check server uptime', 'Gather server info', 'List repo files', 'Disable external repos', 'Enable external repos', 'Patch servers', 'Reboot servers', 'Exit']
+
     while True:
         stdscr.clear()
-        print_menu(stdscr, selected_row_idx)
+        print_menu(stdscr, selected_row_idx, menu)
 
         # Adding live system date and time under the title
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -63,6 +64,17 @@ def main(stdscr):
 
         stdscr.refresh()
         time.sleep(1)  # Refresh every second
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and selected_row_idx > 0:
+            selected_row_idx -= 1
+        elif key == curses.KEY_DOWN and selected_row_idx < len(menu) - 1:
+            selected_row_idx += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            # Handle the selected menu option here
+            if menu[selected_row_idx] == 'Exit':
+                break
 
 if __name__ == "__main__":
     curses.wrapper(main)
