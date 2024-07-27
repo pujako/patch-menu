8320c7439c2e5b7a203c568c1ea1e6e0623581a7
pujako <arrazzaq.zahari@gmail.com>
2024-07-27 15:41:34 +0800

    .

--- patch-menu_v1/main.py
+++ patch-menu_v1/main.py
@@ -18,50 +18,43 @@ def main(stdscr):
     curses.start_color()
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    selected_row_idx = 0
-    selected_col_idx = 0  # 0 for the left column, 1 for the right column
+    current_row = 0
     server_list = []
 
     while True:
-        insight_menu, action_menu = print_menu(stdscr, selected_row_idx, selected_col_idx)
+        menu = print_menu(stdscr, current_row)
 
         key = stdscr.getch()
 
-        max_menu_len = max(len(insight_menu), len(action_menu))
-        
-        if key == curses.KEY_UP:
-            selected_row_idx = (selected_row_idx - 1) % max_menu_len
-        elif key == curses.KEY_DOWN:
-            selected_row_idx = (selected_row_idx + 1) % max_menu_len
-        elif key == curses.KEY_LEFT:
-            selected_col_idx = (selected_col_idx - 1) % 2
-        elif key == curses.KEY_RIGHT:
-            selected_col_idx = (selected_col_idx + 1) % 2
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(menu) - 1:
+            current_row += 1
         elif key == curses.KEY_ENTER or key == 10:
-            if selected_col_idx == 0:
-                if selected_row_idx == 0:  # Enter server list
-                    server_list = get_server_details(stdscr)
-                elif selected_row_idx == 1:  # List servers
-                    list_servers(stdscr, server_list)
-                elif selected_row_idx == 2:  # Check server uptime
-                    check_server_uptime(stdscr, server_list)
-                elif selected_row_idx == 3:  # Check Oracle DB Status
-                    check_oracle_db_status(stdscr, server_list)
-                elif selected_row_idx == 4:  # Gather server info
-                    gather_server_info(stdscr, server_list)
-                elif selected_row_idx == 5:  # List repo files
-                    list_repo_files(stdscr, server_list)
-            elif selected_col_idx == 1:
-                if selected_row_idx == 0:  # Disable external repos
-                    disable_external_repos(stdscr, server_list)
-                elif selected_row_idx == 1:  # Enable external repos
-                    enable_external_repos(stdscr, server_list)
-                elif selected_row_idx == 2:  # Patch servers
-                    patch_server(stdscr, server_list)
-                elif selected_row_idx == 3:  # Reboot servers
-                    bounce_server(stdscr, server_list)
-                elif selected_row_idx == 4:  # Exit
-                    break  # Exit
+            if current_row == 0:  # Enter server list
+                server_list = get_server_details(stdscr)
+            elif current_row == 1:  # List servers
+                list_servers(stdscr, server_list)
+            elif current_row == 2:  # Check server uptime
+                check_server_uptime(stdscr, server_list)
+            elif current_row == 3:  # Check Oracle DB Status
+                check_oracle_db_status(stdscr, server_list)
+            elif current_row == 4:  # Gather server info
+                gather_server_info (stdscr, server_list)
+            elif current_row == 5:  # List repo files
+                list_repo_files(stdscr, server_list)
+            elif current_row == 6:  # Disable external repos
+                disable_external_repos(stdscr, server_list)
+            elif current_row == 7:  # Enable external repos
+                enable_external_repos(stdscr, server_list)
+            elif current_row == 8:  # Patch servers
+                patch_server(stdscr, server_list)
+            elif current_row == 8:  # Reboot servers
+                bounce_server(stdscr, server_list)
+            elif current_row == 10:  # Exit
+                break
+
+        menu = print_menu(stdscr, current_row)
 
     curses.endwin()
 
--- patch-menu_v1/utils/print_menu.py
+++ patch-menu_v1/utils/print_menu.py
@@ -1,7 +1,7 @@
 import curses
 from datetime import datetime
 
-def print_menu(stdscr, selected_row_idx, selected_col_idx):
+def print_menu(stdscr, selected_row_idx):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
 
@@ -31,37 +31,23 @@ def print_menu(stdscr, selected_row_idx, selected_col_idx):
     stdscr.addstr(title_y + 2, date_time_x, now)
 
     # Defining the menu items
-    insight_menu = ['Enter server list', 'List servers', 'Check server uptime', 'Check Oracle DB Status', 'Gather server info', 'List repo files']
-    action_menu = ['Disable external repos', 'Enable external repos', 'Patch servers', 'Reboot servers', 'Exit']
+    menu = ['Enter server list', 'List servers', 'Check server uptime', 'Check Oracle DB Status', 'Gather server info', 'List repo files', 'Disable external repos', 'Enable external repos', 'Patch servers', 'Reboot servers', 'Exit']
 
-    menu_start_y = h // 2 - max(len(insight_menu), len(action_menu)) // 2 + 4
-
-    for idx, row in enumerate(insight_menu):
-        x = w // 4 - len(row) // 2
-        y = menu_start_y + idx
-        if selected_col_idx == 0 and idx == selected_row_idx:
+    for idx, row in enumerate(menu):
+        x = w // 2 - len(row) // 2
+        y = h // 2 - len(menu) // 2 + idx + 2 + 2  # +2 to account for the title, +2 for the date and time
+        if idx == selected_row_idx:
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y, x, row)
             stdscr.attroff(curses.color_pair(1))
         else:
             stdscr.addstr(y, x, row)
 
-    for idx, row in enumerate(action_menu):
-        x = 3 * w // 4 - len(row) // 2
-        y = menu_start_y + idx
-        if selected_col_idx == 1 and idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, row)
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            stdscr.addstr(y, x, row)
-
-    # Commented out initials
     # Adding initials at the bottom centered
-    # initials = "pujako"
-    # initials_x = w // 2 - len(initials) // 2
-    # initials_y = h - 2  # Positioning the initials at the bottom
-    # stdscr.addstr(initials_y, initials_x, initials)
+    initials = "pujako"
+    initials_x = w // 2 - len(initials) // 2
+    initials_y = h - 2  # Positioning the initials at the bottom
+    stdscr.addstr(initials_y, initials_x, initials)
 
     stdscr.refresh()
-    return insight_menu, action_menu
+    return menu
