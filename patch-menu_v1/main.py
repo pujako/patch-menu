88ab6930cd0a605265a76f0ecd3bd8ee5be6ddf5
pujako <arrazzaq.zahari@gmail.com>
2024-07-27 14:47:11 +0800

    .

--- patch-menu_v1/main.py
+++ patch-menu_v1/main.py
@@ -18,43 +18,60 @@ def main(stdscr):
     curses.start_color()
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    current_row = 0
+    selected_row_idx = 0
+    selected_col_idx = 0  # 0 for the left column, 1 for the right column
     server_list = []
 
     while True:
-        menu = print_menu(stdscr, current_row)
+        insight_menu, action_menu, exit_option = print_menu(stdscr, selected_row_idx, selected_col_idx)
 
         key = stdscr.getch()
 
-        if key == curses.KEY_UP and current_row > 0:
-            current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < len(menu) - 1:
-            current_row += 1
+        if key == curses.KEY_UP:
+            if selected_col_idx == 0:
+                selected_row_idx = (selected_row_idx - 1) % (len(insight_menu) + 1)
+            else:
+                selected_row_idx = (selected_row_idx - 1) % (len(action_menu))
+        elif key == curses.KEY_DOWN:
+            if selected_col_idx == 0:
+                selected_row_idx = (selected_row_idx + 1) % (len(insight_menu) + 1)
+            else:
+                selected_row_idx = (selected_row_idx + 1) % (len(action_menu))
+        elif key == curses.KEY_LEFT:
+            selected_col_idx = 0
+            if selected_row_idx >= len(insight_menu):
+                selected_row_idx = 0
+        elif key == curses.KEY_RIGHT:
+            selected_col_idx = 1
+            if selected_row_idx >= len(action_menu):
+                selected_row_idx = 0
         elif key == curses.KEY_ENTER or key == 10:
-            if current_row == 0:  # Enter server list
-                server_list = get_server_details(stdscr)
-            elif current_row == 1:  # List servers
-                list_servers(stdscr, server_list)
-            elif current_row == 2:  # Check server uptime
-                check_server_uptime(stdscr, server_list)
-            elif current_row == 3:  # Check Oracle DB Status
-                check_oracle_db_status(stdscr, server_list)
-            elif current_row == 4:  # Gather server info
-                gather_server_info (stdscr, server_list)
-            elif current_row == 5:  # List repo files
-                list_repo_files(stdscr, server_list)
-            elif current_row == 6:  # Disable external repos
-                disable_external_repos(stdscr, server_list)
-            elif current_row == 7:  # Enable external repos
-                enable_external_repos(stdscr, server_list)
-            elif current_row == 8:  # Patch servers
-                patch_server(stdscr, server_list)
-            elif current_row == 8:  # Reboot servers
-                bounce_server(stdscr, server_list)
-            elif current_row == 10:  # Exit
-                break
+            if selected_col_idx == 0:
+                if selected_row_idx == 0:  # Enter server list
+                    server_list = get_server_details(stdscr)
+                elif selected_row_idx == 1:  # List servers
+                    list_servers(stdscr, server_list)
+                elif selected_row_idx == 2:  # Check server uptime
+                    check_server_uptime(stdscr, server_list)
+                elif selected_row_idx == 3:  # Check Oracle DB Status
+                    check_oracle_db_status(stdscr, server_list)
+                elif selected_row_idx == 4:  # Gather server info
+                    gather_server_info(stdscr, server_list)
+                elif selected_row_idx == 5:  # List repo files
+                    list_repo_files(stdscr, server_list)
+                elif selected_row_idx == len(insight_menu):  # Exit
+                    break
+            else:
+                if selected_row_idx == 0:  # Disable external repos
+                    disable_external_repos(stdscr, server_list)
+                elif selected_row_idx == 1:  # Enable external repos
+                    enable_external_repos(stdscr, server_list)
+                elif selected_row_idx == 2:  # Patch servers
+                    patch_server(stdscr, server_list)
+                elif selected_row_idx == 3:  # Reboot servers
+                    bounce_server(stdscr, server_list)
 
-        menu = print_menu(stdscr, current_row)
+        insight_menu, action_menu, exit_option = print_menu(stdscr, selected_row_idx, selected_col_idx)
 
     curses.endwin()
 
